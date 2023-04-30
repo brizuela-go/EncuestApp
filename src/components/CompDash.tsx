f3ae6583d9707001d7c61452005617fb8b0ad14a
Juan Brizuela <juanbrizuelago@gmail.com>
2023-04-29 18:10:46 -0600

    globe animations, data

--- src/components/CompDash.tsx
+++ src/components/CompDash.tsx
@@ -2,10 +2,14 @@ import LastWeek from "./homeDashboard/LastWeek";
 import RespRecib from "./homeDashboard/ResponsesCard";
 import TablaInicio from "./homeDashboard/HomeTable";
 
-import { useEffect, useRef, useState, lazy, Suspense } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 
 let Globe: any = () => null;
 if (typeof window !== "undefined") Globe = require("react-globe.gl").default;
+
+const ARC_REL_LEN = 0.4;
+const FLIGHT_TIME = 1000;
+const RINGS_MAX_R = 3;
 
 const CompDash = () => {
   const [isClient, setIsClient] = useState(false);
@@ -23,11 +27,48 @@ const CompDash = () => {
   const globeEl = useRef<any>();
 
   useEffect(() => {
-    if (globeEl.current) {
-      const controls = globeEl.current.controls();
-      controls.autoRotate = true;
-      controls.autoRotateSpeed = 0.8;
+    if (isClient) {
+      if (globeEl.current) {
+        const controls = globeEl.current.controls();
+        controls.autoRotate = true;
+        controls.autoRotateSpeed = 0.8;
+        globeEl.current.pointOfView({ lat: 0, lng: 280, altitude: 1.7 });
+      }
     }
+  }, [isClient, globeEl]);
+
+  const gData = [
+    {
+      lat: -5,
+      lng: -97,
+    },
+  ];
+
+  const hexData = [
+    {
+      lat: -5,
+      lng: -97,
+      label: "Mexico",
+    },
+  ];
+
+  const [arcsData, setArcsData] = useState<any>([]);
+
+  const prevCoords = useRef({ lat: 0, lng: 0 });
+  const emitArc = useCallback(({ lat: endLat, lng: endLng }: any) => {
+    const { lat: startLat, lng: startLng } = prevCoords.current;
+    prevCoords.current = { lat: endLat, lng: endLng };
+
+    // add and remove arc after 1 cycle
+    const arc = { startLat, startLng, endLat, endLng };
+    setArcsData((curArcsData: any) => [...curArcsData, arc]);
+    setTimeout(
+      () =>
+        setArcsData((curArcsData: any) =>
+          curArcsData.filter((d: any) => d !== arc)
+        ),
+      FLIGHT_TIME * 2
+    );
   }, []);
 
   return (
@@ -51,6 +92,25 @@ const CompDash = () => {
               backgroundColor="rgba(0,0,0,0)"
               width={700}
               height={600}
+              ringsData={gData}
+              ringColor={() => "rgb(29%, 63%, 77%)"}
+              onGlobeClick={emitArc}
+              arcsData={arcsData}
+              arcColor={() => "rgb(57%, 57%, 93%)"}
+              arcDashLength={ARC_REL_LEN}
+              arcDashGap={2}
+              arcDashInitialGap={1}
+              arcDashAnimateTime={FLIGHT_TIME}
+              arcsTransitionDuration={0}
+              ringMaxRadius={RINGS_MAX_R}
+              pointsData={hexData}
+              pointColor={() => "rgb(44%, 35%, 95%)"}
+              pointResolution={2}
+              pointLabel={({ label }: any) =>
+                `<div align="center" style="background-color: rgba(0, 0, 0, 0.4); padding: 1rem"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/1200px-Flag_of_Mexico.svg.png" align="center" width="40px" height="40px" /> <br /> <p> <b>${label}</b> (${19}°, ${102.36}°) <br /> <b> Usuarios: </b> ${13}</p> </div> `
+              }
+              pointAltitude={0.4}
+              pointRadius={0.9}
             />
           </div>
         )}
