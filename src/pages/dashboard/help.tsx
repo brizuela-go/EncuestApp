3c1844294e5215b48ad204f5d3198ae93454d581
stephano1111 <stephanods@hotmail.com>
2023-04-30 21:18:25 -0600

    faqs 2.0

--- src/pages/dashboard/help.tsx
+++ src/pages/dashboard/help.tsx
@@ -10,178 +10,46 @@ const help: NextPage<Props> = () => {
       description="Preguntas Frecuentes y Ayuda"
     >
       <h1 className="dashboard-title">Ayuda</h1>
-      <section className="mb-32 text-gray-800">
-        <div className="container mx-auto text-center lg:text-left xl:px-32">
-          <div className="grid items-center  lg:grid-cols-2">
-            <div className="mb-12 lg:mb-0">
-              <div
-                className="block rounded-lg px-6 py-12 shadow-lg md:px-12 lg:-mr-14 lg:py-6 xl:py-12"
-                style={{
-                  background: "hsla(0, 0%, 100%, 0.55)",
-                  backdropFilter: "blur(30px)",
-                }}
-              >
-                <h3 className="mb-3 text-2xl font-bold">
-                  We know how valuable your time is
-                </h3>
-                <h5 className="mb-12 text-lg font-bold text-blue-600 lg:mb-10 xl:mb-12">
-                  Let us answer your questions
-                </h5>
-
-                <section className="bg-white dark:bg-gray-900">
-                  <div className="container mx-auto max-w-4xl px-6 py-10">
-                    <h1 className="text-center text-2xl font-semibold text-gray-800 dark:text-white lg:text-3xl">
-                      Frequently asked questions
-                    </h1>
-
-                    <div className="mt-12 space-y-8">
-                      <div className="rounded-lg border-2 border-gray-100 dark:border-gray-700">
-                        <button className="flex w-full items-center justify-between p-8">
-                          <h1 className="font-semibold text-gray-700 dark:text-white">
-                            How i can play for my appoinment ?
-                          </h1>
-
-                          <span className="rounded-full bg-gray-200 text-gray-400">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="h-6 w-6"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M18 12H6"
-                              />
-                            </svg>
-                          </span>
-                        </button>
-
-                        <hr className="border-gray-200 dark:border-gray-700" />
-
-                        <p className="p-8 text-sm text-gray-500 dark:text-gray-300">
-                          Lorem ipsum dolor sit, amet consectetur adipisicing
-                          elit. Voluptas eaque nobis, fugit odit omnis fugiat
-                          deleniti animi ab maxime cum laboriosam recusandae
-                          facere dolorum veniam quia pariatur obcaecati illo
-                          ducimus?
-                        </p>
-                      </div>
-
-                      <div className="rounded-lg border-2 border-gray-100 dark:border-gray-700">
-                        <button className="flex w-full items-center justify-between p-8">
-                          <h1 className="font-semibold text-gray-700 dark:text-white">
-                            Is the cost of the appoinment covered by private
-                            health insurance?
-                          </h1>
-
-                          <span className="rounded-full bg-blue-500 text-white">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="h-6 w-6"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                              />
-                            </svg>
-                          </span>
-                        </button>
-                      </div>
-
-                      <div className="rounded-lg border-2 border-gray-100 dark:border-gray-700">
-                        <button className="flex w-full items-center justify-between p-8">
-                          <h1 className="font-semibold text-gray-700 dark:text-white">
-                            Do i need a referral?
-                          </h1>
-
-                          <span className="rounded-full bg-blue-500 text-white">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="h-6 w-6"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                              />
-                            </svg>
-                          </span>
-                        </button>
-                      </div>
-
-                      <div className="rounded-lg border-2 border-gray-100 dark:border-gray-700">
-                        <button className="flex w-full items-center justify-between p-8">
-                          <h1 className="font-semibold text-gray-700 dark:text-white">
-                            What are your opening house?
-                          </h1>
-
-                          <span className="rounded-full bg-blue-500 text-white">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="h-6 w-6"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                              />
-                            </svg>
-                          </span>
-                        </button>
-                      </div>
-
-                      <div className="rounded-lg border-2 border-gray-100 dark:border-gray-700">
-                        <button className="flex w-full items-center justify-between p-8">
-                          <h1 className="font-semibold text-gray-700 dark:text-white">
-                            What can i expect at my first consultation?
-                          </h1>
-
-                          <span className="rounded-full bg-blue-500 text-white">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="h-6 w-6"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                              />
-                            </svg>
-                          </span>
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </section>
-              </div>
-            </div>
-            <div>
-              <img
-                src="https://mdbootstrap.com/img/new/ecommerce/vertical/075.jpg"
-                className="w-full rounded-lg shadow-lg"
-                alt=""
-              />
-            </div>
+      <section className="dark:bg-gray-800 dark:text-gray-100">
+        <div className="container mx-auto flex flex-col justify-center px-4 py-8 md:p-8">
+          <h2 className="text-2xl font-semibold sm:text-4xl">
+            Frequently Asked Questions
+          </h2>
+          <p className="mt-4 mb-8 dark:text-gray-400">
+            Sagittis tempor donec id vestibulum viverra. Neque condimentum
+            primis orci at lacus amet bibendum.
+          </p>
+          <div className="space-y-4">
+            <details className="w-full rounded-lg border">
+              <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
+                Ex orci laoreet egestas sapien magna egestas scelerisque?
+              </summary>
+              <p className="ml-4 -mt-4 px-4 py-6 pt-0 dark:text-gray-400">
+                Lectus iaculis orci metus vitae ligula dictum per. Nisl per
+                nullam taciti at adipiscing est.{" "}
+              </p>
+            </details>
+            <details className="w-full rounded-lg border">
+              <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
+                Lorem at arcu rutrum viverra metus sapien venenatis lobortis
+                odio?
+              </summary>
+              <p className="ml-4 -mt-4 px-4 py-6 pt-0 dark:text-gray-400">
+                Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna
+                porttitor egestas tincidunt neque vehicula potenti.{" "}
+              </p>
+            </details>
+            <details className="w-full rounded-lg border">
+              <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
+                Eleifend feugiat sollicitudin laoreet adipiscing bibendum
+                suscipit erat?
+              </summary>
+              <p className="ml-4 -mt-4 px-4 py-6 pt-0 dark:text-gray-400">
+                Justo libero tellus integer tincidunt justo semper consequat
+                venenatis aliquet imperdiet. Ultricies urna proin fusce nulla
+                pretium sodales vel magna et massa euismod vulputate sed.{" "}
+              </p>
+            </details>
           </div>
         </div>
       </section>
