87d58fbcf3d3e890b8ba51b1d77d27dd69562467
abraham_mh <xabr12@gmail.com>
2023-04-30 21:01:39 -0600

    my profile updt

--- src/pages/dashboard/myprofile.tsx
+++ src/pages/dashboard/myprofile.tsx
@@ -4,17 +4,21 @@ import { NextPage } from "next";
 import firebase from "../../firebase/firebaseClient";
 import { useAuthState } from "react-firebase-hooks/auth";
 
+import { AiFillGoogleCircle, AiOutlineGithub } from "react-icons/ai";
+import { BsFacebook } from "react-icons/bs";
+
 import Image from "next/image";
 
 type Props = {};
 
 const MyProfile: NextPage<Props> = () => {
   const [user, userLoading] = useAuthState(firebase.auth());
+  console.log(user);
 
   return (
     <DashboardLayout title="Mi Perfil" description="Mi Perfil">
       <div className="p-10">
-        <div className="mt-24 bg-white p-8 shadow">
+        <div className="card mt-16 bg-slate-300 bg-opacity-20 p-8 shadow dark:bg-slate-900 dark:bg-opacity-50">
           <div className="grid grid-cols-1 md:grid-cols-3">
             <div className="order-last mt-20 grid grid-cols-3 text-center md:order-first md:mt-0"></div>
             <div className="relative">
@@ -27,9 +31,9 @@ const MyProfile: NextPage<Props> = () => {
                     fill="currentColor"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     />
                   </svg>
                 )}
@@ -48,34 +52,37 @@ const MyProfile: NextPage<Props> = () => {
             </div>
 
             <div className="mt-32 flex justify-between space-x-8 md:mt-0 md:justify-center">
-              <button className="transform rounded bg-blue-400 py-2 px-4 font-medium uppercase text-white shadow transition hover:-translate-y-0.5 hover:bg-blue-500 hover:shadow-lg">
-                Connect
-              </button>
-              <button className="transform rounded bg-gray-700 py-2 px-4 font-medium uppercase text-white shadow transition hover:-translate-y-0.5 hover:bg-gray-800 hover:shadow-lg">
-                Message
+              <button className="btn-primary btn">Editar</button>
+              <button className="btn bg-gray-700 hover:bg-red-600">
+                Eliminar cuenta
               </button>
             </div>
           </div>
 
           <div className="mt-20 border-b pb-12 text-center">
-            <h1 className="text-4xl font-medium text-gray-700">
+            <h1 className="text-4xl font-medium text-gray-700 dark:text-white">
               {user?.displayName}{" "}
             </h1>
-            <p className="mt-3 font-light text-gray-600">México, Puebla</p>
-
-            <p className="mt-8 text-gray-500">{user?.email}</p>
-          </div>
-
-          <div className="mt-12 flex flex-col justify-center">
-            <p className="text-center font-light text-gray-600 lg:px-16">
-              An artist of considerable range, Ryan — the name taken by
-              Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
-              and records all of his own music, giving it a warm, intimate feel
-              with a solid groove structure. An artist of considerable range.
+            <p className="mt-3 font-light text-gray-600 dark:text-white">
+              México, Puebla
             </p>
-            <button className="mt-4 py-2 px-4  font-medium text-indigo-500">
-              Show more
-            </button>
+            <p className="mt-8 text-gray-500 dark:text-white">
+              {user?.providerData[0]?.providerId === "google.com" && (
+                <AiFillGoogleCircle className="mx-auto text-4xl" />
+              )}
+              {user?.providerData[0]?.providerId === "github.com" && (
+                <AiOutlineGithub className="mx-auto text-4xl" />
+              )}
+              {user?.providerData[0]?.providerId === "facebook.com" && (
+                <BsFacebook className="mx-auto text-3xl" />
+              )}
+            </p>
+            <p className="mt-4 text-gray-500 dark:text-white">{user?.email}</p>
+          </div>
+          <div className="mt-12 flex flex-col justify-center">
+            <p className="lg:px-16dark:text-white text-center font-light text-gray-600 dark:text-white">
+              Description.
+            </p>
           </div>
         </div>
       </div>
