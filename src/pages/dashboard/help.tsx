a6c24ffad30c054b8118a939e97b789e5449cbe2
Juan Brizuela <juanbrizuelago@gmail.com>
2023-05-02 00:33:07 -0600

    profile and help design

--- src/pages/dashboard/help.tsx
+++ src/pages/dashboard/help.tsx
@@ -5,8 +5,41 @@ import {
   withAuthUser,
   withAuthUserTokenSSR,
 } from "next-firebase-auth";
+import Link from "next/link";
+import { FaWhatsapp } from "react-icons/fa";
 
 type Props = {};
+
+const faqs = [
+  {
+    question: "¿Cómo puedo crear una encuesta en la aplicación?",
+    answer: `Para crear una encuesta en la aplicación, debes ir al panel de
+    control de usuario y seleccionar la opción "Crear nueva
+    encuesta". Luego, sigue los pasos guiados para establecer el
+    título de la encuesta, las preguntas y las opciones de
+    respuesta.`,
+  },
+  {
+    question: "¿Puedo personalizar el diseño de mi encuesta?",
+    answer: `Sí, puedes personalizar el diseño de tu encuesta. La aplicación
+    ofrece varias opciones de personalización, incluyendo diferentes
+    temas de diseño, colores, imágenes y fuentes de texto.`,
+  },
+  {
+    question: "¿Puedo compartir mi encuesta con otras personas?",
+    answer: `Sí, puedes compartir tu encuesta con otras personas. La
+    aplicación ofrece varias opciones de compartir, incluyendo
+    compartir en redes sociales, correo electrónico, enlaces
+    directos y códigos de incrustación.`,
+  },
+  {
+    question: "¿Puedo ver los resultados de mi encuesta?",
+    answer: `Sí, puedes ver los resultados de tu encuesta. La aplicación
+    ofrece varias opciones de visualización de resultados, incluyendo
+    gráficos de barras, gráficos de pastel, tablas de datos, insights con inteligencia artificial, 
+    e incluso puedes exportar los datos a un archivo CSV.`,
+  },
+];
 
 const Help: NextPage<Props> = () => {
   return (
@@ -14,49 +47,47 @@ const Help: NextPage<Props> = () => {
       title="FAQ y Ayuda"
       description="Preguntas Frecuentes y Ayuda"
     >
-      <section className="p-8 dark:bg-gray-800 dark:text-gray-100">
+      <section className="entrance  card m-3 bg-slate-300 bg-opacity-20 p-6 dark:bg-slate-900  dark:bg-opacity-50 dark:text-gray-100 lg:m-10 lg:p-10">
         <div className="container mx-auto flex flex-col justify-center px-4 py-8 md:p-8">
           <h2 className="text-2xl font-semibold sm:text-4xl">
             Preguntas frecuentes
           </h2>
-          <p className="mt-4 mb-8 dark:text-gray-400">
-            En ecuestApp tenemos distintas dudas que esperamos sean resueltas
-            con las siguientes preguntas.
-          </p>
+          <div className="mt-4 mb-8 dark:text-gray-200">
+            Aquí encontrarás las preguntas más frecuentes sobre la aplicación y
+            sus respuestas. Si no encuentras la respuesta a tu pregunta, puedes
+            contactarnos a través de{" "}
+            <Link
+              href="mailto:182559@iberopuebla.mx"
+              className=" text-sky-500 hover:text-sky-600 "
+            >
+              182030@iberopuebla.mx
+            </Link>{" "}
+            o por medio de nuestro
+            <Link
+              href="https://wa.me/5212223247857"
+              className=" inline-block text-green-500 hover:text-green-600"
+            >
+              {" "}
+              <div className="flex flex-row items-center justify-center gap-x-1">
+                <FaWhatsapp className=" ml-2" />
+                WhatsApp
+              </div>
+            </Link>
+          </div>
           <div className="space-y-4">
-            <details className="w-full rounded-lg border">
-              <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
-                ¿Cómo puedo crear una encuesta en la aplicación?
-              </summary>
-              <p className="ml-4 -mt-4 px-4 py-6 pt-0 dark:text-gray-400">
-                {`Para crear una encuesta en la aplicación, debes ir al panel de
-                control de usuario y seleccionar la opción "Crear nueva
-                encuesta". Luego, sigue los pasos guiados para establecer el
-                título de la encuesta, las preguntas y las opciones de
-                respuesta.`}{" "}
-              </p>
-            </details>
-            <details className="w-full rounded-lg border">
-              <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
-                ¿Puedo personalizar el diseño de mi encuesta?
-              </summary>
-              <p className="ml-4 -mt-4 px-4 py-6 pt-0 dark:text-gray-400">
-                Sí, puedes personalizar el diseño de tu encuesta. La aplicación
-                ofrece varias opciones de personalización, incluyendo diferentes
-                temas de diseño, colores, imágenes y fuentes de texto.{" "}
-              </p>
-            </details>
-            <details className="w-full rounded-lg border">
-              <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
-                ¿Puedo compartir mi encuesta con otras personas?
-              </summary>
-              <p className="ml-4 -mt-4 px-4 py-6 pt-0 dark:text-gray-400">
-                Sí, puedes compartir tu encuesta con otras personas. La
-                aplicación ofrece varias opciones de compartir, incluyendo
-                compartir en redes sociales, correo electrónico, enlaces
-                directos y códigos de incrustación.{" "}
-              </p>
-            </details>
+            {faqs.map((faq, index) => (
+              <details
+                key={index}
+                className=" group w-full cursor-pointer rounded-lg border border-slate-300 shadow-md transition duration-300 ease-in-out hover:shadow-lg  dark:border-slate-700 "
+              >
+                <summary className="px-4 py-6 font-semibold transition duration-300 ease-in-out focus:outline-none focus-visible:ring-violet-400 group-hover:bg-gradient-to-r  group-hover:text-indigo-800 dark:group-hover:text-indigo-600  ">
+                  {faq.question}
+                </summary>
+                <p className="ml-4 -mt-4 px-4 py-6 pt-0 dark:text-sky-100">
+                  {faq.answer}
+                </p>
+              </details>
+            ))}
           </div>
         </div>
       </section>
@@ -83,6 +114,4 @@ export const getServerSideProps = withAuthUserTokenSSR({
   };
 });
 
-export default withAuthUser<any>({
-  whenAuthed: AuthAction.RENDER,
-})(Help);
+export default withAuthUser<any>()(Help);
--- src/pages/dashboard/myprofile.tsx
+++ src/pages/dashboard/myprofile.tsx
@@ -27,7 +27,7 @@ const MyProfile: NextPage<Props> = () => {
           <div className="grid grid-cols-1 md:grid-cols-3">
             <div className="order-last mt-20 grid grid-cols-3 text-center md:order-first md:mt-0"></div>
             <div className="relative">
-              <div className="absolute inset-x-0 top-0 mx-auto -mt-24 flex h-48 w-48 items-center justify-center rounded-full bg-indigo-100 text-indigo-500 shadow-2xl">
+              <div className="absolute inset-x-0 top-0 mx-auto -mt-24 flex h-40 w-40 items-center justify-center rounded-full bg-indigo-100 text-indigo-500 shadow-2xl">
                 {!user && (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -46,8 +46,8 @@ const MyProfile: NextPage<Props> = () => {
                   <Image
                     src={user && user?.photoURL}
                     alt="Profile"
-                    width={400}
-                    height={400}
+                    width={500}
+                    height={500}
                     quality={100}
                     priority
                     className="rounded-full"
@@ -56,22 +56,24 @@ const MyProfile: NextPage<Props> = () => {
               </div>
             </div>
 
-            <div className="mt-32 flex justify-between space-x-8 md:mt-0 md:justify-center">
-              <button className="btn-primary btn">Editar</button>
-              <button className="btn bg-gray-700 hover:bg-red-600">
+            <div className="mt-32 flex flex-col place-items-center justify-between space-y-5 md:mt-0 md:flex-row md:justify-center md:space-y-0 md:space-x-8">
+              <button className="btn-primary btn shadow-lg hover:shadow-xl">
+                Editar Perfil
+              </button>
+              <button className="btn border-none bg-gray-700 shadow-lg hover:bg-red-500 hover:shadow-xl dark:text-white">
                 Eliminar cuenta
               </button>
             </div>
           </div>
 
           <div className="mt-20 border-b pb-12 text-center">
-            <h1 className="text-4xl font-medium text-gray-700 dark:text-white">
+            <h1 className="text-4xl font-medium text-slate-900 dark:text-white">
               {user?.displayName}{" "}
             </h1>
-            <p className="mt-3 font-light text-gray-600 dark:text-white">
-              México, Puebla
+            <p className="mt-3 font-light text-slate-800 dark:text-white">
+              🇲🇽 México, Puebla
             </p>
-            <p className="mt-8 text-gray-500 dark:text-white">
+            <p className="mt-8 text-slate-700 dark:text-white">
               {user?.providerData[0]?.providerId === "google.com" && (
                 <AiFillGoogleCircle className="mx-auto text-4xl" />
               )}
@@ -79,15 +81,21 @@ const MyProfile: NextPage<Props> = () => {
                 <AiOutlineGithub className="mx-auto text-4xl" />
               )}
               {user?.providerData[0]?.providerId === "facebook.com" && (
-                <BsFacebook className="mx-auto text-3xl" />
+                <BsFacebook className="mx-auto text-4xl" />
               )}
             </p>
-            <p className="mt-4 text-gray-500 dark:text-white">{user?.email}</p>
+            <p className="mt-4 text-gray-700 dark:text-white">{user?.email}</p>
           </div>
-          <div className="mt-12 flex flex-col justify-center">
-            <p className="lg:px-16dark:text-white text-center font-light text-gray-600 dark:text-white">
-              Description.
+          <div className="mt-12 flex flex-col justify-center space-y-8">
+            <p className="lg:px-16dark:text-white text-center font-light text-gray-800 dark:text-white">
+              Descripción
             </p>
+            <button
+              className="btn-outline  btn-accent btn mx-auto w-1/2 justify-center shadow-lg hover:shadow-xl
+            "
+            >
+              Generar Descripción con IA 🖊️ 🤖
+            </button>
           </div>
         </div>
       </div>
